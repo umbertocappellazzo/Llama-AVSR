fe6eefdcf2326160eac4b6f3d5d4a33bb4404434
umbertocappellazzo <umbertocappellazzo@gmail.com>
2025-02-28 00:55:13 +0100

    Changed repos.

--- models/lightning_LLM.py
+++ models/lightning_LLM.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("..")
 import torch
 import torchaudio
-from ..utils.cosine import WarmupCosineScheduler
+from utils.cosine import WarmupCosineScheduler
 from pytorch_lightning import LightningModule
 from transformers import AutoTokenizer
 from Llama_LoRA import LoRA_config
